d3d64a0769829f35fa26e08cf93dba0fc65511ab
pedrosqr1 <127567823+pedrosqr1@users.noreply.github.com>
2023-04-03 20:19:54 -0300

    Add files via upload

---
+++ ex7.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace borabora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //7) Solicitar um número entre 1 e 4. Se a pessoa digitar um número diferente, mostrar a mensagem 
+            //"entrada inválida" e solicitar o número novamente.Se digitar correto mostrar o número digitado
+            int num;
+            Console.WriteLine("Insira um número entre 1 e 4: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            while ((num<1)||(num>4))
+            {
+                Console.WriteLine("Entrada inválida. Insira um número entre 1 e 4: ");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            
+                Console.WriteLine($"Número Digitado: {num}");
+            
+        }
+    }
+}
