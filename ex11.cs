f44fc069673295b8fe1867b984ae1dcc4f3dcbac
pedrosqr1 <127567823+pedrosqr1@users.noreply.github.com>
2023-04-03 20:55:43 -0300

    Add files via upload

---
+++ ex11.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace aesf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escreva um programa que solicita 10 números ao usuário, através de um laço while, e ao final 
+            //mostre qual destes números é o maior.
+
+            int i = 1;
+            int num;
+            int maior;
+            int menor;
+          
+            
+            Console.WriteLine("digite a bosta de um numero: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            maior = num;
+            menor = num;
+            while (i<10)
+            {
+                Console.WriteLine("digite a bosta de um numero: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num < menor)
+                {
+                    menor = num;
+                }
+                if (num > maior)
+                {
+                    maior = num;
+                }
+                if (i == 10)
+                    break;
+                i++;
+                           
+            }
+            
+            Console.WriteLine("O maior número é: " + maior);
+            Console.WriteLine("O maior número é: " + menor);
+
+        }
+    }
+}
