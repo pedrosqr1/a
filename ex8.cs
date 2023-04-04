48b8cc6c1994119620c5752afcde8fe333a9e0dd
pedrosqr1 <127567823+pedrosqr1@users.noreply.github.com>
2023-04-03 21:53:11 -0300

    Add files via upload

---
+++ ex8.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ex8feito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nome;
+            Console.WriteLine("nome: ");
+            nome = Console.ReadLine();
+
+            foreach(char c in nome)
+            {
+                Console.Write(c);
+            }
+            Console.WriteLine(Environment.NewLine+"FOR 0 ate N");
+            for (int i = 0; i < nome.Length; i++)
+            {
+                char c = nome[i];
+                Console.Write(c);
+            }
+            Console.WriteLine(Environment.NewLine+"FOR N ate 0");
+            for (int i = nome.Length-1;i>=0;i--)
+            {
+                char c = nome[i];
+                Console.Write(c);
+            }
+          
+        }
+    }
+}
