0b87716bf2515269ac9ccb69fc68d10b5d225b9b
pedrosqr1 <127567823+pedrosqr1@users.noreply.github.com>
2023-04-03 21:05:16 -0300

    Add files via upload

---
+++ ex3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumEntrada = 0;
+            int Produto = 1;
+            do
+            {
+
+                Console.WriteLine("Entre com um número: ");
+                NumEntrada = Convert.ToInt32(Console.ReadLine());
+
+                if (NumEntrada == 0)
+                    break;
+                Produto *= NumEntrada;
+            } while (NumEntrada != 0);
+            Console.WriteLine($"Produto: {Produto}");
+        }
+    }
+}
