63fb485e3e3c31d4dd84943e9247da5f71b9af25
pedrosqr1 <127567823+pedrosqr1@users.noreply.github.com>
2023-04-03 22:19:31 -0300

    Add files via upload

---
+++ ex10.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ex10fazendo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int i = 1;
+            int notas;
+            int media= 0;
+            Console.WriteLine("Insira o número de alunos: ");
+
+            num = Convert.ToInt32(Console.ReadLine());
+            
+            while(i<=num)
+            {
+                Console.WriteLine("Digite as notas dos alunos: ");
+                notas = Convert.ToInt32(Console.ReadLine());
+                if (i == 10)
+                    break;
+                i++;
+                media += notas;
+            }
+            Console.WriteLine($"Média dos alunos: {media/num}");
+        }
+    }
+}
