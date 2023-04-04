c0a48ef0fbd19c794e9cccafce0b6bb314256e75
pedrosqr1 <127567823+pedrosqr1@users.noreply.github.com>
2023-04-03 21:24:03 -0300

    Add files via upload

---
+++ ex5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ex5fazendo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int num;
+            int maior;
+            int menor;
+
+
+            Console.WriteLine("digite a bosta de um numero: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            maior = num;
+            menor = num;
+            while (i < 15)
+            {
+                Console.WriteLine("digite a bosta de um numero: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num < menor)
+                {
+                    menor = num;
+                }
+                if (num > maior)
+                {
+                    maior = num;
+                }
+                if (i == 15)
+                    break;
+                i++;
+
+            }
+
+            Console.WriteLine("O maior número é: " + maior);
+            Console.WriteLine("O maior número é: " + menor);
+
+        }
+    }
+}
