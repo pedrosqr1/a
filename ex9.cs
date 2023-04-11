da463057ff36de904632ef3bb705631bad6375a7
pedrosqr1 <127567823+pedrosqr1@users.noreply.github.com>
2023-04-10 21:36:31 -0300

    Add files via upload

---
+++ ex9.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ex13tentando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //9) Escreva um aplicativo mostra todos os números ímpares de 1 até 100.
+            int NumImparAtual = -1;
+            for (int i = 1; i <= 50;i++)
+            {
+               Console.WriteLine(NumImparAtual+2);
+               
+               NumImparAtual += 2;
+            }
+        }
+    }
+}
