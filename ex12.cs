52e7baf8119b88ab15776697a05469fa79031387
pedrosqr1 <127567823+pedrosqr1@users.noreply.github.com>
2023-04-10 22:02:54 -0300

    Add files via upload

---
+++ ex12.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ex12temtadp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            int num = 0;
+            while(num<100)
+            {
+                num += 1;
+                Console.WriteLine(num);
+            }
+
+            int a = 1;
+
+            do
+            {
+                Console.WriteLine(a);
+                a++;
+
+            } while (a <=100);
+        }
+    }
+}
